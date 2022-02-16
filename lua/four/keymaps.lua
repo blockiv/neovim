62428fa323fe2c6e72227c3a2fcd51661e2d723e
blockiv <blockiv@tuta.io>
2022-02-16 00:58:01 -0500

    cleanup

--- lua/four/keymaps.lua
+++ lua/four/keymaps.lua
@@ -64,4 +64,3 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 -- Plugins --
 -- Telescope
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-
