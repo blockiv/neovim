2120c9e37e61220a7d0b4fa54e1025e0b6fc0d45
blockiv <blockiv@tuta.io>
2022-02-16 03:13:20 -0500

    Enable catppuccin integration with ts-rainbow

--- lua/four/colorscheme.lua
+++ lua/four/colorscheme.lua
@@ -1,7 +1,14 @@
 local colorscheme = "catppuccin"
 
+require"catppuccin".setup{
+    integrations = {
+        ts_rainbow = true,
+    }
+}
+
+-- Attempt to load colorscheme
 local success, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not success then
-    vim.notify("Colorscheme " .. colorscheme .. " not found!")
+    vim.notify("Catppuccin not found!")
     return
 end
