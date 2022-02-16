63c1ccc2d8817d0718c55595b94289aea036b583
blockiv <blockiv@tuta.io>
2022-02-16 00:18:41 -0500

    Add support for telescope plugin

--- lua/four/keymaps.lua
+++ lua/four/keymaps.lua
@@ -60,3 +60,8 @@ keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Plugins --
+-- Telescope
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+
