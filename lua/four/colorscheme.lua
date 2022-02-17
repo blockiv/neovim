cc84d015cfe536312ac3ce00935256f4fa95e90e
blockiv <blockiv@tuta.io>
2022-02-16 19:23:24 -0500

    Explicitly enable catppuccin integrations

--- lua/four/colorscheme.lua
+++ lua/four/colorscheme.lua
@@ -1,14 +1,20 @@
 local colorscheme = "catppuccin"
 
-require"catppuccin".setup{
-    integrations = {
-        ts_rainbow = true,
-    }
-}
+require("catppuccin").setup({
+	integrations = {
+		treesitter = true,
+		ts_rainbow = true,
+		cmp = true,
+		gitsigns = true,
+		telescope = true,
+		nvimtree = { enabled = true },
+		bufferline = true,
+	},
+})
 
 -- Attempt to load colorscheme
 local success, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not success then
-    vim.notify("Catppuccin not found!")
-    return
+	vim.notify("Catppuccin not found!")
+	return
 end
